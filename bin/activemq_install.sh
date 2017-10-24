74231dcda8d4c46f28c79a4b643571964078b5a0
Travis Sanders <travsanders@gmail.com>
2017-10-24 15:29:56 -0600

    updated for v4

--- bin/activemq_install.sh
+++ bin/activemq_install.sh
@@ -13,7 +13,12 @@ rm apache-activemq-5.14.5-bin.tar.gz
 cp /home/caretaker/pfun/conf/activemq/activemq.xml /opt/apache-activemq-5.14.5/conf/activemq.xml
 
 keytool -import -alias "My CA" -file /etc/puppetlabs/puppet/ssl/certs/ca.pem -keystore truststore.jks
-cat /etc/puppetlabs/puppet/ssl/certs/puppetmaster.pbcorp.photobucket.com.pem /etc/puppetlabs/puppet/ssl/private_keys/puppetmaster.pbcorp.photobucket.com.pem > temp.pem
+#version 5.x
+#cat /etc/puppetlabs/puppet/ssl/certs/puppetmaster.pbcorp.photobucket.com.pem /etc/puppetlabs/puppet/ssl/private_keys/puppetmaster.pbcorp.photobucket.com.pem > temp.pem
+
+#version 4.x
+cat /etc/puppetlabs/puppet/ssl/certs/puppetmaster.domain.pem /etc/puppetlabs/puppet/ssl/private_keys/puppetmaster.domain.pem > temp.pem
+
 openssl pkcs12 -export -in temp.pem -out activemq.p12 -name activemq.example.com
 keytool -importkeystore  -destkeystore keystore.jks -srckeystore activemq.p12 -srcstoretype PKCS12 -alias activemq.example.com
 cp keystore.jks truststore.jks /opt/apache-activemq-5.14.5/conf/

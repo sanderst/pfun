f36dae0b835fdf98f6940308cd17e46b0a8cb76f
Travis Sanders <travsanders@gmail.com>
2017-10-24 15:37:01 -0600

    udpated permissions

--- bin/mcollective/mcollective_client.sh
+++ bin/mcollective/mcollective_client.sh
@@ -4,10 +4,15 @@ mkdir /home/caretaker/.mcollective.d
 mkdir -p ~/.mcollective.d/credentials/certs
 mkdir -p ~/.mcollective.d/credentials/private_keys
 
+sudo puppet cert generate mcollective-servers
 
-cp /etc/puppetlabs/puppet/ssl/certs/puppetmaster.pbcorp.photobucket.com.pem /home/caretaker/.mcollective.d/credentials/certs/admin_cert.pem
+# Version 5.x
+#cp /etc/puppetlabs/puppet/ssl/certs/puppetmaster.pbcorp.photobucket.com.pem /home/caretaker/.mcollective.d/credentials/certs/admin_cert.pem
+#cp /etc/puppetlabs/puppet/ssl/private_keys/puppetmaster.pbcorp.photobucket.com.pem /home/caretaker/.mcollective.d/credentials/private_keys/admin_private.pem
 
-cp /etc/puppetlabs/puppet/ssl/private_keys/puppetmaster.pbcorp.photobucket.com.pem /home/caretaker/.mcollective.d/credentials/private_keys/admin_private.pem
+#Version 4.x
+cp /etc/puppetlabs/puppet/ssl/certs/puppetmaster.domain.pem /home/caretaker/.mcollective.d/credentials/certs/admin_cert.pem
+cp /etc/puppetlabs/puppet/ssl/private_keys/puppetmaster.domain.pem /home/caretaker/.mcollective.d/credentials/private_keys/admin_private.pem
 
 cp /etc/puppetlabs/puppet/ssl/certs/mcollective-servers.pem /home/caretaker/.mcollective.d/credentials/certs/mcollective-servers.pem
 

a15a93811c91d4a6cca6cd0f864e7a84912680c6
Travis <travsanders@gmail.com>
2017-10-24 07:02:57 -0600

    mcollective client config

---
+++ bin/mcollective_client.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir /home/caretaker/.mcollective.d
+mkdir -p ~/.mcollective.d/credentials/certs
+mkdir -p ~/.mcollective.d/credentials/private_keys
+
+
+cp /etc/puppetlabs/puppet/ssl/certs/puppetmaster.pbcorp.photobucket.com.pem /home/caretaker/.mcollective.d/credentials/certs/admin_cert.pem
+
+cp /etc/puppetlabs/puppet/ssl/private_keys/puppetmaster.pbcorp.photobucket.com.pem /home/caretaker/.mcollective.d/credentials/private_keys/admin_private.pem
+
+cp /etc/puppetlabs/puppet/ssl/certs/mcollective-servers.pem /home/caretaker/.mcollective.d/credentials/certs/mcollective-servers.pem
+
+cp /home/caretaker/pfun/conf/mcollective/client/client.cfg /etc/puppetlabs/mcollective/client.cfg
+
+systemctl stop mcollective
+systemctl start mcollective

315d3e5ff10261440923fdf504f50a8385e45566
Travis <travsanders@gmail.com>
2017-10-23 22:09:44 -0600

    modified cert generation

--- bin/generate_certs.sh
+++ bin/generate_certs.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-cp /etc/puppetlabs/puppet/ssl/certs/ca.pem .
-cp /etc/puppetlabs/puppet/ssl/certs/puppetmaster.pbcorp.photobucket.com.pem amq_cert.pem
-cp /etc/puppetlabs/puppet/ssl/private_keys/puppetmaster.pbco
-rp.photobucket.com.pem amq_private.pem
+mkdir /home/caretaker/sandbox
+
+cp /etc/puppetlabs/puppet/ssl/certs/ca.pem /home/caretaker/sandbox/ca.pem
+cp /etc/puppetlabs/puppet/ssl/certs/puppetmaster.pbcorp.photobucket.com.pem /home/caretaker/sandbox/amq_cert.pem
+cp /etc/puppetlabs/puppet/ssl/private_keys/puppetmaster.pbcorp.photobucket.com.pem /home/caretaker/sandbox/amq_private.pem
+
 /opt/puppetlabs/bin/puppet cert generate mcollective-servers
-cp /etc/puppetlabs/puppet/ssl/certs/mcollective-servers.pem shared_cert.pem
-cp /etc/puppetlabs/puppet/ssl/private_keys/mcollective-servers.pem shared_private.pem
-cp /etc/puppetlabs/puppet/ssl/certs/puppetmaster.pbcorp.photobucket.com.pem admin_cert.pem
-cp /etc/puppetlabs/puppet/ssl/private_keys/puppetmaster.pbcorp.photobucket.com.pem admin_private.pem
+
+cp /etc/puppetlabs/puppet/ssl/certs/mcollective-servers.pem /home/caretaker/sandbox/shared_cert.pem
+cp /etc/puppetlabs/puppet/ssl/private_keys/mcollective-servers.pem /home/caretaker/sandbox/shared_private.pem
+cp /etc/puppetlabs/puppet/ssl/certs/puppetmaster.pbcorp.photobucket.com.pem /home/caretaker/sandbox/admin_cert.pem
+cp /etc/puppetlabs/puppet/ssl/private_keys/puppetmaster.pbcorp.photobucket.com.pem /home/caretaker/sandbox/admin_private.pem
+
+ssh 10.1.5.21 'mkdir /home/caretaker/sandbox'
+scp /home/caretaker/sandbox/* caretaker@10.1.5.21:~/sandbox/
